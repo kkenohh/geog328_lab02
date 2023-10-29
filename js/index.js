f4e79785af4eb722a1989310f14513bd3e07cb85
Ken <kenoh17@uw.edu>
2023-10-28 23:05:58 -0700

    done with website

--- js/index.js
+++ js/index.js
@@ -18,3 +18,18 @@ accordions.forEach(accordion => accordion.addEventListener("click", (e) => {
     post.style.display = "flex";
   }
 }));
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("mySlides");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000);
+}
