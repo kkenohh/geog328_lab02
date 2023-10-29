5c6250a494aec626d74abc81b1b6f2ccde875acc
Ken <kenoh17@uw.edu>
2023-10-28 21:25:18 -0700

    finished blog posts page

--- js/index.js
+++ js/index.js
@@ -2,9 +2,19 @@ const hamburger = document.querySelector('#hamburger');
 const bars = document.querySelectorAll('.bars');
 const menu = document.querySelector('.nav-links');
 
-function changeHamburger(e) {
+hamburger.addEventListener('click', () => {
   bars.forEach(bar => bar.classList.toggle('transform-bars'))
   menu.classList.toggle('open');
-}
+});
 
-hamburger.addEventListener('click', changeHamburger)
+const accordions = document.querySelectorAll(".accordion");
+
+accordions.forEach(accordion => accordion.addEventListener("click", (e) => {
+  e.currentTarget.classList.toggle('active');
+  let post = e.currentTarget.nextElementSibling;
+  if (post.style.display === "flex") {
+    post.style.display = "none";
+  } else {
+    post.style.display = "flex";
+  }
+}));
